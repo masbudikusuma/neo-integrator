79f67c41364d8e2df2149f9ceaf3ed84c2d31fa6
masbudikusuma <58821556+masbudikusuma@users.noreply.github.com>
2022-05-12 08:36:12 +0700

    sample config
    rename config-sample.php menjadi config.php

---
+++ component/config-sample.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once 'component/version.php';
+$weburl = "http://neo-in/neo-in/";
+  
+// Konfigurasi Database Neo Integrator 
+$db_server   = "";
+$db_username = "";
+$db_password = "";
+$db_database = "";
+$db = mysqli_connect($db_server,$db_username,$db_password,$db_database);
+
+include_once 'component/ws_pddikti.php';
+
+// AKUN PDDIKTI / NEO-FEEDER
+$urlfeeder  = "";   // contoh http://192.168.1.1:8100
+$userfeeder = "";   // User PDDIKTI
+$passfeeder = "";   // Pass PDDIKTI
+$ws = new Ws_pddikti($urlfeeder."/ws/live2.php",$userfeeder,$passfeeder);
+
+date_default_timezone_set("Asia/Bangkok");
+$date = date('Y-m-d h:i:s', time());
+$id_insert = time();
+
+include_once 'component/ws_pddikti.php';
+include_once 'component/function.php';
+ini_set('max_execution_time', '100000');
+
+?>
